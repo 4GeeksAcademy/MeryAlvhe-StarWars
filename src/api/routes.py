fae014650097906b6ce3bbeedfef12ed93a70dca
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-03 19:06:04 +0000

    modified:   Pipfile 	modified:   Pipfile.lock 	modified:   src/api/routes.py 	modified:   src/app.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,6 +4,9 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import get_jwt_identity
 from api.models import db, Users
 
 api = Blueprint('api', __name__)
@@ -14,4 +17,35 @@ CORS(api) # Allow CORS requests to this API
 def handle_hello():
     response_body = {}
     response_body ['message'] = "Hello! I'm a message that came from the backend"
+    return response_body, 200
+
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def login():
+    response_body = {}
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Lógica de validación de e-mail y contraseña
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password ==password))
+    if user:
+        access_token = create_access_token(identity=email)
+        response_body['message'] = "login succesfull"
+        response_body['access_token'] = access_token
+        return response_body, 200
+    response_body['message'] = "Bad email or password"
+    return response_body, 401
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/profile", methods=["GET"])
+@jwt_required()
+def protected():
+    response_body = {}
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    print(current_user)
+    response_body['message'] = f"user logueado: {current_user}"
     return response_body, 200
