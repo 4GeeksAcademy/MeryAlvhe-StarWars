b9d4b7a8cf485e6f8c3b2a0f4cd87e2cb3e600be
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-06-03 21:06:39 +0000

    modified:   migrations/env.py 	renamed:    migrations/versions/1ad471cac1c6_.py -> migrations/versions/cacea158eea9_.py 	deleted:    migrations/versions/fe34110b22df_.py 	modified:   src/api/admin.py 	modified:   src/api/models.py

--- src/api/models.py
+++ src/api/models.py
@@ -9,6 +9,8 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=False)
+    last_name = db.Column(db.String(), unique=False, nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,4 +19,26 @@ class Users(db.Model):
         # Do not serialize the password, its a security breach
         return {'id': self.id,
                 'email': self.email,
-                'is_active': self.is_active}
+                'is_active': self.is_active,
+                'first_name': self.first_name,
+                'last_name': self.last_name}
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=False)
+    body = db.Column(db.String(), nullable=False)
+    image_url= db.Column(db.String(), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user_to = db.relationship('Users', foreign_keys=[user_id])
+
+    def __repr__(self):
+        return f'<Post: {self.title}>'
+
+    def serialize(self):
+        return {'id': self.id,
+                'title': self.title,
+                'description': self.description,
+                'body' : self.body,
+                'imaage_url': self.image_url,
+                'user_id': self.user_id}
