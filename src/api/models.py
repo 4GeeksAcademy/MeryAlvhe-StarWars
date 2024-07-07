3affbb30d4ab739979c429aef34c5e81f4129dce
Mery Álvarez <159366647+Meryalvhe@users.noreply.github.com>
2024-07-07 09:20:28 +0000

    avances

--- src/api/models.py
+++ src/api/models.py
@@ -9,14 +9,13 @@ class Users(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    first_name = db.Column(db.String(), unique=False, nullable=False)
-    last_name = db.Column(db.String(), unique=False, nullable=False)
+    first_name = db.Column(db.String(), unique=False, nullable=True)
+    last_name = db.Column(db.String(), unique=False, nullable=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
 
     def serialize(self):
-        # Do not serialize the password, its a security breach
         return {'id': self.id,
                 'email': self.email,
                 'is_active': self.is_active,
--- src/api/routes.py
+++ src/api/routes.py
@@ -22,20 +22,118 @@ def handle_hello():
 
 # Create a route to authenticate your users and return JWTs. The
 # create_access_token() function is used to actually generate the JWT.
-@api.route("/login", methods=["POST"])
-def login():
+@api.route('/signup', methods=['GET', 'POST'])
+def signup():
     response_body = {}
-    email = request.json.get("email", None)
+    email = request.json.get("email", None).lower()
     password = request.json.get("password", None)
-    # Lógica de validación de e-mail y contraseña
-    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password ==password))
-    if user:
-        access_token = create_access_token(identity=email)
-        response_body['message'] = "login succesfull"
+    user = Users()
+    user.email = email
+    user.password = password
+    user.is_active = True
+    user.is_admin = False
+    db.session.add(user)
+    db.session.commit()
+    access_token = create_access_token(identity={ 'user_id': user.id,
+                                                  'user_is_admin': user.is_admin})
+    response_body['message'] = 'User Signed Up'
+    response_body['access_token'] = access_token
+    response_body['results']=user.serialize()
+    return response_body, 200
+
+
+@api.route('/login', methods=['POST'])
+def login(): 
+    response_body = {}
+    email = request.json.get("email", None).lower()
+    password = request.json.get("password", None)
+    user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    print(user)
+    if user: 
+        access_token = create_access_token(identity={ 'user_id': user.id,
+                                                      'user_is_admin': user.is_admin})
+        response_body['message'] = 'User logged in'
         response_body['access_token'] = access_token
+        response_body['results'] = user.serialize()
+        # Nos hace falta devolver los favoritos del usuario. Incluyendo el ID del favorito y el ID del criminal.
+        rows = db.session.execute(db.select(SavedCriminals).where(SavedCriminals.user_id == user.id)).scalars()
+        results = [row.public_serialize() for row in rows]
+        response_body['saved_criminals'] = results
         return response_body, 200
-    response_body['message'] = "Bad email or password"
+    response_body['message'] = 'Wrong Username Or Password'
     return response_body, 401
+
+
+@api.route('/users', methods=['GET'])
+def handle_users():
+    response_body = {}
+    rows = db.session.execute(db.select(Users)).scalars()
+    results = [row.serialize() for row in rows]
+    response_body['results'] = results
+    response_body['message'] = 'User List'
+    return response_body, 200
+
+
+@api.route('/users/<int:user_id>', methods=['GET', 'DELETE', 'PUT'])
+def handle_users_id(user_id):
+    response_body = {}
+    if request.method == 'GET':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            response_body['results'] = user.serialize()
+            response_body['message'] = 'User Found'
+            return response_body, 200
+        response_body['message'] = 'User Not Found'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'PUT':
+        data = request.json
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            user.email = data['email']
+            user.is_active = data['is_active']
+            user.name = data['name']
+            user.surname = data['surname']
+            user.description = data['description']
+            user.avatar = data['avatar']
+            db.session.commit()
+            response_body['message'] = 'Datos del usuario actualizados'
+            response_body['results'] = user.serialize()
+            return response_body, 200
+        response_body['message'] = 'User Profile Updated'
+        response_body['results'] = {}
+        return response_body, 404
+    if request.method == 'DELETE':
+        user = db.session.execute(db.select(Users).where(Users.id == user_id)).scalar()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            response_body['message'] = 'User Deleted'
+            response_body['results'] = {}
+            return response_body, 200
+        response_body['message'] = 'User Not Found'
+        response_body['results'] = {}
+        return response_body, 404  
+
+@api.route('/users/<int:user_id>/favorites-Characters', methods=['GET'])
+def handle_users_favorites_Characters(user_id):
+    response_body = {}
+    saved_favorites_characters = db.session.execute(db.select(CharacterFavorites).where(CharacterFavorites.user_id == user_id)).scalars()
+    results = [row.serialize() for row in saved_favorites_characters]
+    response_body['results'] = results
+    response_body['message'] = 'Saved favorites characters'
+    return response_body, 200
+
+@api.route('/users/<int:user_id>/favorites-planets', methods=['GET'])
+def handle_users_favorites_planets(user_id):
+    response_body = {}
+    saved_favorites_planets = db.session.execute(db.select(PlanetFavorites).where(PlanetFavorites.user_id == user_id)).scalars()
+    results = [row.serialize() for row in saved_favorites_characters]
+    response_body['results'] = results 
+    response_body['message'] = 'Saved favorites Planets'
+    return response_body, 200
+
+
 
 
 # Protect a route with jwt_required, which will kick out requests
